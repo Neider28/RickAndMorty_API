264043204119aa5cc03c5f2e7706b01b7f958b79
neider28 <neidersilva2802@gmail.com>
2022-06-10 15:00:57 -0500

    Add Loading animation

--- js/index.js
+++ js/index.js
@@ -6,13 +6,25 @@ const maxPageForAPI = Math.floor(Math.random() * 26);;
 const apiPage = '?page=:page';
 const characters = [];
 
+const accionAsincrona = async () => {
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
+      resolve();
+  }, 3000);
+});   
+}
+
 const getDataRickAndMorty = async (page) => {
   try {
+      await accionAsincrona();
+
       const { results } = await fetchData(`${API}${apiPage.replace(':page', `${page}`)}`)
       results.map(character => {
         const card = cardGenerator(character);
         cardsContent.appendChild(card);
       });
+
+      document.getElementById("spinner").style.display="none";
   } catch (error) {
     console.error(error);
   }
