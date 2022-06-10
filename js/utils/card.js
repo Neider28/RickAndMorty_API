95f14b3e82d6380f2da8d03a57846077404a23ef
neider28 <neidersilva2802@gmail.com>
2022-06-10 11:41:04 -0500

    JavaScript change

--- js/utils/card.js
+++ js/utils/card.js
@@ -1,18 +1,18 @@
 const cardGenerator = object => {
-    const card = document.createElement('article');
-    card.classList.add('card')
-  
-    card.innerHTML = `
-    <div class="card-image">
-      <img class="img-fluid" src="${object.image}" alt="imagen de ${object.name}">
-    </div>
-    <div class="card-body">
-    <span class="${object.status.toLowerCase()}">${object.status}</span>
-      <h3>${object.name}</h3>
-      <p>${object.species}</p>
-      <p>${object.location.name}</p>
-    </div>
-    `;
-  
-    return card;
-  }
+  const card = document.createElement('article');
+  card.classList.add('card')
+
+  card.innerHTML = `
+  <div class="card-image">
+    <img class="img-fluid" src="${object.image}" alt="imagen de ${object.name}">
+  </div>
+  <div class="card-body">
+  <span class="${object.status.toLowerCase()}">${object.status}</span>
+    <h3>${object.name}</h3>
+    <p>${object.species}</p>
+    <p>${object.location.name}</p>
+  </div>
+  `;
+
+  return card;
+}
